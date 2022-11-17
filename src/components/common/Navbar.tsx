554b0f4fc36c62553522a3550b816e0b7223f655
helenliuzzo <helen.liuzzo@gmail.com>
2022-11-17 12:46:23 +0100

    styling home

---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navigation-bar">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/usersignin" className="btns">
+              SignUp
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
