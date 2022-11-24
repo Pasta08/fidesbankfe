c2916680958e44ce8095cd6a4044f7c3f878365e
Pasta08 <ethanbone435@gmail.com>
2022-11-24 15:38:30 +0100

    added rest api call to user Login

--- src/pages/UserLogIn.tsx
+++ src/pages/UserLogIn.tsx
@@ -2,25 +2,55 @@ import React, { useState } from "react";
 import { Form } from "react-router-dom";
 
 const UserLogIn = () => {
-  const [UserLogin] = useState({
-    email: "",
-    password: "",
-  });
+	const [user, setUser] = useState({
+		email: "",
+		password: "",
+	});
 
-  return (
-    <form>
-      <label>
-        <p>email</p>
-        <input type={"email"} />
-      </label>
-      <label>
-        <p>password</p>
-        <input type={"password"} />
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-  );
+	const updateUser = (key: string, value: any) => {
+		setUser({ ...user, [key]: value });
+		console.log(user);
+	};
+
+	const sendForm = async (event: any) => {
+		event.preventDefault();
+		fetch("http://localhost:8080/springbank/api/v1/login/bank-user", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				mode: "no-cors",
+			},
+			body: JSON.stringify(user),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("Success:", data);
+			})
+			.then((error) => {
+				console.log("Error", error);
+			});
+	};
+
+	return (
+		<form onSubmit={sendForm}>
+			<input
+				type="email"
+				className="input"
+				placeholder="email"
+				name="email"
+				onChange={(e) => updateUser("email", e.target.value)}
+			/>
+			<input
+				type="password"
+				className="input"
+				placeholder="password"
+				name="password"
+				onChange={(e) => updateUser("password", e.target.value)}
+			/>
+			<div>
+				<button type="submit">Submit</button>
+			</div>
+		</form>
+	);
 };
 export default UserLogIn;
