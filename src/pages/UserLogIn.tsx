e685e337c2194bffd65883bd0ba51450fffa48df
AnitaAgostini <anitaagostini96@gmail.com>
2022-12-05 11:47:32 +0100

    update and new variable UserLogiIn

--- src/pages/UserLogIn.tsx
+++ src/pages/UserLogIn.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { Form } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
+import "../Styles/UserLogin.css";
+import "../components/Navbar";
 
 const UserLogIn = () => {
 	const [user, setUser] = useState({
@@ -32,25 +34,42 @@ const UserLogIn = () => {
 	};
 
 	return (
-		<form onSubmit={sendForm}>
+		<body>
+            <section>
+				<form onSubmit={sendForm}>
+			<div className="App">
+			<div className="container-md">
+				<h2>Login Page</h2>
+			<label >
+			<h3 className="row-container">Email</h3>
 			<input
 				type="email"
 				className="input"
 				placeholder="email"
 				name="email"
 				onChange={(e) => updateUser("email", e.target.value)}
-			/>
+			/></label></div>
+            <div  className="container-md">
+				<label>
+			  <h3>Password</h3>
 			<input
 				type="password"
 				className="input"
 				placeholder="password"
 				name="password"
 				onChange={(e) => updateUser("password", e.target.value)}
-			/>
+			/></label> </div>
 			<div>
-				<button type="submit">Submit</button>
+				<button className='btn_form' type="submit">Submit</button>
+				<p className="forgot-password text-right">
+				Forgot <a href="#">Password</a>
+			</p>
+			<p>Not Registered ?<Link  to="/usersignin">Create account Now!</Link></p>
 			</div>
-		</form>
+			</div>
+			</form>
+			</section>
+           </body>		
 	);
 };
 export default UserLogIn;
